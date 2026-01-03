6ffe4c79ac5c19fda735de7044382cb09992b09b
Ethan James <necrozma@catgirlsaresexy.org>
2026-01-03 16:41:55 -0700

    Add license header to Main.java
    Added license information to Main.java

--- src/main/java/xyz/necrozma/upnp/Main.java
+++ src/main/java/xyz/necrozma/upnp/Main.java
@@ -1,3 +1,17 @@
+/*
+ * This file is part of UPnP.
+ *
+ * UPnP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 only.
+ *
+ * UPnP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ */
+
 package xyz.necrozma.upnp;
 
 import dev.dejvokep.boostedyaml.route.Route;
